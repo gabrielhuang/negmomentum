93a56bff095cb682c8d91375289b1ffbeb865be8
Gabriel Huang <gbxhuang@gmail.com>
2018-05-16 00:49:02 -0400

    write proper launch script

--- dcgan.sh
+++ dcgan.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-RUNDIR="dcgan"
-shift 0
+FORMULATION=$1
+GP=$2
+shift 2
+RUNDIR="$FORMULATION-$GP"
 
-python gabriel_main.py "log/$RUNDIR" --dataset cifar10 --dataroot cifar $@  # get remaining args
+python gabriel_main.py "log/$RUNDIR" --formulation $FORMULATION --gp-type $GP --dataset cifar10 --dataroot cifar --log-every 10 $@  # get remaining args
