fb0e8ad1cb66b6f25f734f04e6e71d3f1a074cc2
Gabriel Huang <gbxhuang@gmail.com>
2018-05-16 18:13:31 -0400

    change default dir

--- dcgan.sh
+++ dcgan.sh
@@ -2,6 +2,6 @@
 FORMULATION=$1
 GP=$2
 shift 2
-RUNDIR="$FORMULATION-$GP"
+RUNDIR="$HOME/m/nmlog/$FORMULATION-$GP"
 
-python gabriel_main.py "log/$RUNDIR" --formulation $FORMULATION --gp-type $GP --dataset cifar10 --dataroot cifar --log-every 10 $@  # get remaining args
+python gabriel_main.py $RUNDIR --formulation $FORMULATION --gp-type $GP --dataset cifar10 --dataroot cifar --log-every 10 $@  # get remaining args
