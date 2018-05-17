8d6c9f530d5056af1b1930a4af8e7a65dd3602ae
Gabriel Huang <gbxhuang@gmail.com>
2018-05-16 23:00:29 -0400

    change default gp

--- dcgan.sh
+++ dcgan.sh
@@ -2,6 +2,8 @@
 FORMULATION=$1
 GP=$2
 shift 2
-RUNDIR="$HOME/m/nmlog/$FORMULATION-$GP"
 
-python gabriel_main.py $RUNDIR --formulation $FORMULATION --gp-type $GP --dataset cifar10 --dataroot cifar --log-every 10 $@  # get remaining args
+PENALTY=1.
+RUNDIR="$HOME/m/nmlog/$FORMULATION-$GP$PENALTY"
+
+python gabriel_main.py $RUNDIR --gp $PENALTY --formulation $FORMULATION --gp-type $GP --dataset cifar10 --dataroot cifar --log-every 10 $@  # get remaining args
