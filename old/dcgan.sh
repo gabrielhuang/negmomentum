86b0272815f836412e36b14dfa60bed47db839b4
Gabriel Huang <gbxhuang@gmail.com>
2020-05-19 12:26:41 -0400

    changes

--- old/dcgan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FORMULATION=$1
-GP=$2
-shift 2
-
-PENALTY=1.
-RUNDIR="$HOME/m/nmlog/$FORMULATION-$GP$PENALTY"
-
-python gabriel_main.py $RUNDIR --gp $PENALTY --formulation $FORMULATION --gp-type $GP --dataset cifar10 --dataroot cifar --log-every 10 $@  # get remaining args
